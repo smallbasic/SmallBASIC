f10bda6d99e2ee7ebc1087a8f6816b0d642fee8f
chrisws <cwarrensmith@gmail.com>
2005-03-20 23:37:42 +0000

    updated cmd_html() and cmd_image() to use par_massget() - much cleaner...

--- src/blib.c
+++ src/blib.c
@@ -2824,72 +2824,36 @@ void	cmd_exprseq(void)
 // HTML html, [title,] [x,y,w,h]
 // Display html text
 //
-void do_cmd_html(var_t* html, var_t* title) {
-   
-    int x=-1, y=-1, w=-1, h=-1;
-
-    par_getstr(html);              if (prog_error) return;
-    if (code_peek() == kwTYPE_SEP) {
-        par_getcomma();            if (prog_error) return;
-        par_getstr(title);
-        if (code_peek() == kwTYPE_SEP) {
-            par_getcomma();        if (prog_error) return;
-            x = par_getint();      if (prog_error) return;
-            par_getcomma();        if (prog_error) return;
-            y = par_getint();      if (prog_error) return;
-            par_getcomma();        if (prog_error) return;
-            w = par_getint();      if (prog_error) return;
-            par_getcomma();        if (prog_error) return;
-            h = par_getint();      if (prog_error) return;
-        }
-    } else {
-        title->v.p.ptr = 0;
-    }
-    dev_html(html->v.p.ptr, title->v.p.ptr, x, y, w, h);
-}
-
 void cmd_html() {
-    var_t html, title;
-    v_init(&html);
-    v_init(&title);
+    char* html = 0;
+    char* title = 0;
+    int x,y,w,h;
 
-    do_cmd_html(&html, &title);
-    v_free(&html);
-    v_free(&title);    
+    x=y=w=h=0;
+    par_massget("Ssiiii", &html, &title, &x, &y, &w, &h);
+    if (!prog_error) {
+        dev_html(html, title, x, y, w, h);
+    }
+    pfree2(html, title);
 }
 
 //
-// IMAGE handle, index, x, y [,sx,sy [,w,h]]
+// IMAGE #handle, index, x, y [,sx,sy [,w,h]]
 // Display html text
 //
 void cmd_image() {
-   
     int h,i,x,y;
     int sx,sy,iw,ih;
     sx =sy =iw =ih =0;
 
-	par_getsharp();        if (prog_error) return;
-    h = par_getint();      if (prog_error) return;
-    par_getcomma();        if (prog_error) return;
-    i = par_getint();      if (prog_error) return;
-    par_getcomma();        if (prog_error) return;
-    x = par_getint();      if (prog_error) return;
-    par_getcomma();        if (prog_error) return;
-    y = par_getint();      if (prog_error) return;
-
-    if (code_peek() == kwTYPE_SEP) {
-        par_getcomma();       if (prog_error) return;
-        sx = par_getint();    if (prog_error) return;
-        par_getcomma();       if (prog_error) return;
-        sy = par_getint();    if (prog_error) return;
-        if (code_peek() == kwTYPE_SEP) {
-            par_getcomma();   if (prog_error) return;
-            iw = par_getint(); if (prog_error) return;
-            par_getcomma();   if (prog_error) return;
-            ih = par_getint(); if (prog_error) return;
-        }
+    par_getsharp();                        
+    if (prog_error) {
+        return;
     }
 
-    dev_image(h, i, x, y, sx, sy, iw, ih);
+    par_massget("IIIIiiii", &h, &i, &x, &y, &sx, &sy, &iw, &ih);
+    if (!prog_error) {
+        dev_image(h, i, x, y, sx, sy, iw, ih);
+    }
 }
 
