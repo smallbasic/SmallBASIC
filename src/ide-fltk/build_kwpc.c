7ac8320698bee32d68c5d1302c806e58512fee0a
chrisws <cwarrensmith@gmail.com>
2005-12-19 11:09:30 +0000

    now generates keyword list from internal strutcture

--- src/ide-fltk/build_kwpc.c
+++ src/ide-fltk/build_kwpc.c
@@ -1,125 +1,111 @@
 /**
-*	builds the kwp.cxx (keword tables)
-*/
+ *   builds the kwp.cxx (keyword tables)
+ */
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-char	line[256];
-char	keyword[256];
+char    line[256];
+char    keyword[256];
 
 typedef struct { char key[64]; } kw_t;
 
-kw_t	ktable[2048];
-int		kcount;
+kw_t    ktable[2048];
+int     kcount;
 
-void	add_key(const char *key)
-{
-	strcpy(ktable[kcount].key, key);
-	kcount ++;
+#define code_t int
+#define fcode_t int
+#define pcode_t int
+#define SEC(x)
+
+struct keyword_s {
+    char    name[16];
+    code_t  code;
+};
+
+struct opr_keyword_s {
+    char    name[16];
+    code_t  code;
+    code_t  opr;
+};
+
+struct func_keyword_s {
+    char    name[16];
+    fcode_t fcode;
+};
+
+struct proc_keyword_s {
+    char    name[16];
+    pcode_t pcode;
+};
+
+struct spopr_keyword_s {
+    char    name[16];
+    code_t  code;
+};
+
+#include "../kw.h"
+#include "../languages/keywords.en.c"
+
+void add_key(const char *key) {
+    strcpy(ktable[kcount].key, key);
+    kcount ++;
 }
 
-int		key_cmp(const void *a, const void *b)
-{
-	return strcasecmp( ((kw_t*) a)->key, ((kw_t*) b)->key );
+int key_cmp(const void *a, const void *b) {
+    return strcasecmp( ((kw_t*) a)->key, ((kw_t*) b)->key );
 }
 
-void	sort_ktable()
-{
-	qsort(ktable, kcount, sizeof(kw_t), key_cmp);
+void sort_ktable() {
+    qsort(ktable, kcount, sizeof(kw_t), key_cmp);
 }
 
-int		line2keyword(const char *src, char *buf)
-{
-	char	*p, *k;
-
-	k = buf;
-	p = (char *) src;
-	while ( *p )	{
-		if	( *p == '\n' )
-			break;
-		*k ++ = *p ++;
-		}
-	*k = '\0';
-
-	k = buf;
-	while ( *k )	{
-		*k = tolower(*k);
-		k ++;
-		}
-
-	return strlen(buf);
+void print_ktable(FILE *fp) {
+    int     i;
+    for ( i = 0; i < kcount; i ++ ) {
+        fprintf(fp, "\"%s\"", ktable[i].key);
+        if (i != kcount - 1) {
+            fprintf(fp, ", ");
+        }
+        if (((i+1) % 8) == 0) {
+            fprintf(fp, "\n");
+        }
+    }
 }
 
-void	print_ktable(FILE *fp)
-{
-	int		i;
+int main(int argc, char *argv[]) {
+    FILE* fo = fopen("kwp.cxx", "wt");
+    fprintf(fo, "/* automagicaly generated file */\n");
 
-	for ( i = 0; i < kcount; i ++ )	{
-		fprintf(fp, "\"%s\"", ktable[i].key);
-		if	( i != kcount - 1)
-			fprintf(fp, ", ");
-		if	( ((i+1) % 8) == 0 )	
-			fprintf(fp, "\n");
-		}
-}
+    kcount = 0;
+    fprintf(fo, "const char *code_keywords[] = { // List of basic level keywords\n");
+    for (int i=0; keyword_table[i].name[0] != '\0'; i++) {
+        if (keyword_table[i].name[0] != '$') {
+            add_key(keyword_table[i].name);
+        }
+    }
+    sort_ktable();
+    print_ktable(fo);
 
-int	main(int argc, char *argv[])
-{
-	FILE	*fp, *fo;
-	int		mode = 0, ignore;
-	int		mode_count = 0;
-	
-	system("sbasic -pkw > kwp");
-	fp = fopen("kwp", "rt");
-	fo = fopen("kwp.cxx", "wt");
+    kcount = 0;
+    fprintf(fo, "};\n\nconst char *code_procedures[] = { // functions\n");
+    for (int i=0; proc_table[i].name[0] != '\0'; i++) {
+        add_key(proc_table[i].name);
+    }
+    sort_ktable();
+    print_ktable(fo);
 
-	//	
-	fprintf(fo, "/* automagicaly generated file */\n");
-	fprintf(fo, "const char *code_keywords[] = { // List of basic level keywords\n");
-	kcount = 0;
-	
-	while ( fgets(line, 256, fp) )	{
-		if	( line2keyword(line, keyword) )	{
-			if	( strcmp(keyword, "and") == 0 )		{
-				mode = 1;
-				mode_count = 0;
-				}
-			if	( strcmp(keyword, "$$$-functions") == 0 )	{
-				mode = 2;
-				sort_ktable();
-				print_ktable(fo);
-			
-				fprintf(fo, "};\n\nconst char *code_functions[] = { // functions\n");
-			
-				kcount = 0;
-				mode_count = 0;
-				}
-			if	( strcmp(keyword, "$$$-procedures") == 0 )	{
-				mode = 2;
-				sort_ktable();
-				print_ktable(fo);
-			
-				fprintf(fo, "};\n\nconst char *code_procedures[] = { // functions\n");
-			
-				kcount = 0;
-				mode_count = 0;
-				}
+    kcount = 0;
+    fprintf(fo, "};\n\nconst char *code_functions[] = { // functions\n");
+    for (int i=0; func_table[i].name[0] != '\0'; i++) {
+        add_key(func_table[i].name);
+    }
+    sort_ktable();
+    print_ktable(fo);
 
-			ignore = (!(isalpha(keyword[0]))) || (mode == 0);
-			if	( !ignore )	
-				add_key(keyword);
-			}
-		}
-		
-	//
-	sort_ktable();
-	print_ktable(fo);
-	fprintf(fo, "};\n");
-		
-	fclose(fo);
-	fclose(fp);
-	return 0;
+    fprintf(fo, "};\n");
+    fclose(fo);
+    return 0;
 }
