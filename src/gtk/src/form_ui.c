d59425c81357e8e0792a7dfc92938319cf3fb007
chrisws <cwarrensmith@gmail.com>
2006-06-21 23:11:28 +0000

    forum_ui.c now compiles but still work in progress

--- src/gtk/src/form_ui.c
+++ src/gtk/src/form_ui.c
@@ -1,5 +1,5 @@
 /* -*- c-file-style: "java" -*-
- * $Id: form_ui.c,v 1.1 2006-06-14 10:52:40 zeeb90au Exp $
+ * $Id: form_ui.c,v 1.2 2006-06-21 23:11:28 zeeb90au Exp $
  * This file is part of SmallBASIC
  *
  * Copyright(C) 2001-2006 Chris Warren-Smith. Gawler, South Australia
@@ -22,133 +22,91 @@
 #include "blib_ui.h"
 
 #include <gtk/gtk.h>
-#include <glib/garray.h>
 #include "output_model.h"
 
 GtkWidget* form = 0; /* modal form */
-GPtrArray* widgets = 0; /* modeless form widgets */
+extern OutputModel output;
 
-enum ControlType {
+typedef enum ControlType {
     ctrl_button,
     ctrl_radio,
     ctrl_check,
     ctrl_text,
     ctrl_label,
     ctrl_list
-};
+} ControlType;
 
-struct WidgetInfo {
+typedef struct WidgetInfo {
     ControlType type;
     var_t* var;
-};
+} WidgetInfo;
 
 WidgetInfo* get_widget_info(GtkWidget* w) {
-    return (WidgetInfo*)gtk_object_get_data(w, "widget_info");    
+    return (WidgetInfo*)g_object_get_data(G_OBJECT(w), "widget_info");
 }
 
-gboolean button_press_event(GtkWidget *widget, GdkEventButton *event) {
+void set_widget_info(GtkWidget* w, WidgetInfo* inf) {
+    g_object_set_data(G_OBJECT(w), "widget_info", inf);
+}
+
+gboolean button_cb(GtkWidget *widget, GdkEventButton *event) {
     WidgetInfo* inf = get_widget_info(widget);
-    //v_setstrn(inf->var, "1", 1);
+    v_setstrn(inf->var, "1", 1);
     //wnd->setModal(false);
     //wnd->penState = 2;
 }
 
-void radio_cb(Widget* w, void* v) {
+void radio_cb(GtkWidget* widget, void* v) {
     WidgetInfo* inf = get_widget_info(widget);
     v_setstrn(inf->var, "1", 1);
 }
 
 // transfer widget data in variables
-void updateVars(GtkWidget* w) {
+void updateVars(GtkWidget* widget) {
     WidgetInfo* inf = get_widget_info(widget);
     switch (inf->type) {
     case ctrl_check:
     case ctrl_radio:
-        if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(w))) {
+        if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget))) {
             v_setstrn(inf->var, "1", 1);
         }
         break;
     case ctrl_text:
         // copy input data into variable
-        v_setstrn(inf->var, ((Input*)w)->value(), ((Input*)w)->size());
+        //v_setstrn(inf->var, ((Input*)w)->value(), ((Input*)w)->size());
         break;
     case ctrl_list:
         // copy drop list item into variable
-        v_setstr(inf->var, ((Choice*)w)->item()->label());
+        //v_setstr(inf->var, ((Choice*)w)->item()->label());
         break;
     default:
         break;
     }
 }
 
-// copy all modeless widget fields into variables
-void updateForm() {
-    if (widgets) {
-        int len = widgets->len;
-        for (int i=0; i<len; i++) {
-            updateVars(g_ptr_array_index(widgets, i));
-        }
-    }
-}
-
-// close the modeless widgets
-void closeModeless() {
-    if (widgets) {
-        int len = widgets->len;
-        for (int i=0; i<len; i++) {
-            GtkWidget* widget = g_ptr_array_index(widgets, i);
-            WidgetInfo* inf = get_widget_info(widget);
-/*             w->parent()->remove(w); */
-/*             w->parent(0); */
-/*             delete inf; */
-/*             delete w; */
-        }
-        g_ptr_array_free(widgets, TRUE);
-        widgets = 0;
-    }
-}
-
-void formBegin() {
-    if (widgets) {
-        ui_reset();
-        return;
-    }
+void form_begin() {
     if (form == 0) {
-        form = gtk_layout_new (NULL, NULL);
-        gtk_widget_show (form);
-        gtk_container_add(GTK_CONTAINER(output.widget), form);
-        gtk_layout_set_size(GTK_LAYOUT(form), output.width, output.height);
+        form = gtk_layout_new(NULL, NULL);
+        gtk_layout_put(GTK_LAYOUT(output.widget), form, 0, 0);
+        printf("new form\n");
     }
 }
-
-void formEnd(Widget* w) {
-    if (widgets) {
-        wnd->outputGroup->end();
-        widgets->add((Object*)w);
-    } else {
-        form->end();
-    }
-}
-
-C_LINKAGE_BEGIN
 
 void ui_reset() {
     if (form != 0) {
-        form->hide();
-        int n = form->children();
-        for (int i=0; i<n; i++) {
-            WidgetInfo* inf = (WidgetInfo*)form->child(i)->user_data();
-            delete inf;
+        GList* list = gtk_container_get_children(GTK_CONTAINER(form));
+        int n = g_list_length(list);
+        int i;
+        for (i=0; i<n; i++) {
+            GtkWidget* w = (GtkWidget*)g_list_nth_data(list, i);
+            WidgetInfo* inf = get_widget_info(w);
+            g_free(inf);
+            gtk_container_remove(GTK_CONTAINER(form), w);
         }
-        form->clear();
-        wnd->outputGroup->remove(form);
-        form->parent(0);
-        delete form;
+        gtk_container_remove(GTK_CONTAINER(output.widget), form);
+        g_list_free(list);
         form = 0;
-
-        wnd->out->show();
-        wnd->out->take_focus();
-        wnd->out->redraw();
+        printf("reset form\n");
     }
 }
 
@@ -166,29 +124,29 @@ void cmd_button() {
     char* type = 0;
 
     if (-1 != par_massget("IIIIPSs", &x, &y, &w, &h, &v, &caption, &type)) {
-        Button* widget = 0;
-        WidgetInfo* inf = new WidgetInfo();
+        GtkWidget* widget = 0;
+        WidgetInfo* inf = (WidgetInfo*)g_malloc(sizeof(WidgetInfo));
         inf->var = v;
 
-        formBegin();
+        form_begin();
         if (type) {
             if (strncmp("radio", type, 5) == 0) {
-                widget = new RadioButton(x, y, w, h);
-                widget->callback(radio_cb);
+                //widget = new RadioButton(x, y, w, h);
+                //widget->callback(radio_cb);
                 inf->type = ctrl_radio;
             } else if (strncmp("checkbox", type, 8) == 0) {
-                widget = new CheckButton(x, y, w, h);
-                widget->callback(radio_cb);
+                //widget = new CheckButton(x, y, w, h);
+                //widget->callback(radio_cb);
                 inf->type = ctrl_check;
             } else if (strncmp("button", type, 6) == 0) {
-                widget = new Button(x, y, w, h);
-                widget->callback(radio_cb);
+                //widget = new Button(x, y, w, h);
+                //widget->callback(radio_cb);
                 inf->type = ctrl_button;
             } else if (strncmp("label", type, 8) == 0) {
-                widget = (Button*)new Widget(x, y, w, h);
-                widget->box(NO_BOX);
+                //widget = (Button*)new GtkWidget(x, y, w, h);
                 inf->type = ctrl_label;
             } else if (strncmp("choice", type, 6) == 0) {
+#if 0
                 Choice* choice = new Choice(x, y, w, h);
                 choice->begin();
                 // "Easy|Medium|Hard"
@@ -210,8 +168,8 @@ void cmd_button() {
                 }
                 choice->user_data(inf);
                 choice->end();
-                formEnd(choice);
                 pfree2(caption, type);
+#endif
                 return;
             } else {
                 ui_reset();
@@ -219,9 +177,9 @@ void cmd_button() {
             }
         }
         if (widget == 0) {
-            widget = new Button(x, y, w, h);
-            widget->callback(button_cb);
-            inf->type = ctrl_button;
+            //widget = new Button(x, y, w, h);
+            //widget->callback(button_cb);
+            //inf->type = ctrl_button;
         }
 
         // prime input field from variable
@@ -229,15 +187,14 @@ void cmd_button() {
             strcmp((const char*)v->v.p.ptr, "1") == 0) {
             if (inf->type == ctrl_check || 
                 inf->type == ctrl_radio) {
-                widget->value(true);
+                //widget->value(true);
             } else if (inf->type != ctrl_button) {
-                widget->value((const char*)v->v.p.ptr);
+                //widget->value((const char*)v->v.p.ptr);
             }
         }
 
-        widget->copy_label(caption);
-        widget->user_data(inf);
-        formEnd(widget);
+        //widget->copy_label(caption);
+        //widget->user_data(inf);
     }
     pfree2(caption, type);
 }
@@ -250,119 +207,95 @@ void cmd_text() {
     var_t* v = 0;
 
     if (-1 != par_massget("IIIIP", &x, &y, &w, &h, &v)) {
-        formBegin();
-        Input* widget = new Input(x, y, w, h);
-        widget->box(BORDER_BOX);
-        widget->color(color(220,220,220));
+        form_begin();
+        GtkWidget* entry = gtk_entry_new();
 
         // prime field from var_t
         if (v->type == V_STR && v->v.p.ptr) {
-            widget->value((const char*)v->v.p.ptr);
+            gtk_entry_set_text(GTK_ENTRY(entry), (const char*)v->v.p.ptr);
         }
-
-        WidgetInfo* inf = new WidgetInfo();
+    
+        //g_object_set(G_OBJECT(entry), "autocap", FALSE, NULL);
+        gtk_layout_put(GTK_LAYOUT(output.widget), entry,
+                       //        gtk_layout_put(GTK_LAYOUT(form), entry,
+                       output.cur_x-1, output.cur_y-1);
+        gtk_entry_set_has_frame(GTK_ENTRY(entry), TRUE);
+        gtk_entry_set_max_length(GTK_ENTRY(entry), 100);
+        gtk_entry_set_width_chars(GTK_ENTRY(entry), 10);
+        gtk_widget_modify_font(entry, output.font_desc);
+        //g_signal_connect(G_OBJECT(entry), "key_press_event",
+        //G_CALLBACK(key_press_event), NULL);
+        //g_signal_connect(G_OBJECT(entry), "changed",
+        //G_CALLBACK(input_changed), entry);
+        //gtk_widget_show(entry);
+        //printf("show widget\n");
+        
+        //GtkIMContext* imctx = gtk_im_multicontext_new();
+        //gtk_im_context_set_client_window(imctx, output.widget->window);
+        //gtk_im_context_focus_in(imctx);
+        //gtk_im_context_show(imctx);
+        gtk_widget_grab_focus(entry);
+   
+        WidgetInfo* inf = (WidgetInfo*)g_malloc(sizeof(WidgetInfo));
         inf->var = v;
         inf->type = ctrl_text;
-        widget->user_data(inf);
-
-        formEnd(widget);
+        set_widget_info(entry, inf);
     }
 }
 
 // DOFORM [x,y,w,h [,border-style, bg-color]]
 // Executes the form
 void cmd_doform() {
-    int x, y, w, h, box, bg;
-    int numArgs;
+    int x, y, w, h;
+    int num_args;
 
-    x = y = w = h = box = bg = 0;
-    numArgs = par_massget("iiiiii", &x, &y, &w, &h, &box, &bg);
-
-    if (numArgs != 0 && numArgs != 1 && (numArgs < 4 || numArgs > 6)) {
-        ui_reset();
-        rt_raise("UI: INVALID FORM ARGUMENTS: %d", numArgs);
+    if (form == 0) {
+        rt_raise("UI: DOFORM called before TEXT or BUTTON.");
         return;
     }
 
-    if (numArgs == 1) {
-        // modeless operations
+    x = y = w = h = 0;
+    num_args = par_massget("iiii", &x, &y, &w, &h);
+
+    if (num_args != 4) {
         ui_reset();
-        switch (x) {
-        case 0: // begin modeless state
-            widgets = new strlib::List(10);
-            break;
-        case 1: // end modeless state
-            updateForm();
-            closeModeless();
-            break;
-        case 2: // transfer form variables
-            updateForm();
-            break;
-        case 3: // close only
-            closeModeless();
-            break;
-        }
+        rt_raise("UI: INVALID FORM ARGUMENTS: %d", num_args);
         return;
-    } else if (numArgs > 0) {
-        switch (box) {
-        case 1:
-            form->box(BORDER_BOX);
-            break;
-        case 2:
-            form->box(SHADOW_BOX);
-            break;
-        case 3:
-            form->box(ENGRAVED_BOX);
-            break;
-        case 4:
-            form->box(THIN_DOWN_BOX);
-            break;
-        default: 
-            form->box(NO_BOX);
-        }
-        if (numArgs == 6) {
-            form->color(AnsiWindow::ansiToFltk(bg));
-        }
-        if (x < 2) {
-            x = 2;
-        }
-        if (y < 2) {
-            y = 2;
-        }
-        form->x(x);
-        form->y(y);
-        if (x+w > form->w()) {
-            w = form->w()-x;
-        }
-        form->w(w);
-        if (y+h > form->h()) {
-            h = form->h()-y;
-        }
-        form->h(h);
-    } else {
-        form->box(ENGRAVED_BOX);
+    }
+    
+    if (x < 2) {
+        x = 2;
+    }
+    if (y < 2) {
+        y = 2;
+    }
+    if (x+w > output.width) {
+        w = output.width-x;
+    }
+    if (y+h > output.height) {
+        h = output.height-y;
     }
 
-    wnd->tabGroup->selected_child(wnd->outputGroup);
-    form->take_focus();
-    form->show();
+    printf("form at %d %d %d %d\n", x,y,w,h);
+    gtk_layout_move(GTK_LAYOUT(output.widget), form, x, y);
+    gtk_layout_set_size(GTK_LAYOUT(form), w, h);
+    gtk_widget_grab_focus(form);
+    gtk_widget_show(form);
 
-    wnd->setModal(true);
-    while (wnd->isModal()) {
-        fltk::wait();
+    output.modal_flag = TRUE;
+    while (output.modal_flag && output.break_exec == 0) {
+        gtk_main_iteration_do(TRUE);
     }
 
-    if (wnd->isBreakExec()) {
-        brun_break();
+    GList* list = gtk_container_get_children(GTK_CONTAINER(form));
+    int n = g_list_length(list);
+    int i;
+    for (i=0; i<n; i++) {
+        updateVars((GtkWidget*)g_list_nth_data(list, i));
     }
-    wnd->resetPen();
-
-    int n = form->children();
-    for (int i=0; i<n; i++) {
-        updateVars(form->child(i));
-    }
+    g_list_free(list);
 
     ui_reset();
 }
 
-/* End of "$Id: form_ui.c,v 1.1 2006-06-14 10:52:40 zeeb90au Exp $". */
+/* End of "$Id: form_ui.c,v 1.2 2006-06-21 23:11:28 zeeb90au Exp $". */
