22d65b03c38b2b7150791207671de8537c121c70
Chris Warren-Smith <cwarrensmith@gmail.com>
2018-08-05 18:32:13 +1000

    ANDROID: fix issue #66

--- src/ui/graphics.h
+++ src/ui/graphics.h
@@ -43,7 +43,7 @@ inline void RGB888_BE_to_RGB(pixel_t c, uint8_t &r, uint8_t &g, uint8_t &b) {
 #if defined(PIXELFORMAT_RGBA8888)
   #define SET_RGB(r, g, b) ((0xff000000) | (r << 16) | (g << 8) | (b))
   #define GET_RGB RGB888_to_RGB
-  #define GET_RGB2 RGB888_BE_to_RGB
+  #define GET_RGB2 RGB888_to_RGB
   #define GET_FROM_RGB888 RGB888_to_RGBA8888
 #else
   #define SET_RGB(r, g, b) ((r << 16) | (g << 8) | (b))
