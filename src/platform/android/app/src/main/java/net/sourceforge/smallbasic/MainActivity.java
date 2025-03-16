593a6459252e9bc6e3ef8e229f261e1bda8bdfb7
chrisws <cwarrensmith@gmail.com>
2025-03-16 17:35:19 +1030

    ANDROID: only show immersive mode when gesture mode is enabled

--- src/platform/android/app/src/main/java/net/sourceforge/smallbasic/MainActivity.java
+++ src/platform/android/app/src/main/java/net/sourceforge/smallbasic/MainActivity.java
@@ -24,6 +24,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.Settings;
 import android.util.Base64;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -834,6 +835,17 @@ public class MainActivity extends NativeActivity {
     }
   }
 
+  private boolean isGestureNavigationEnabled() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      try {
+        return Settings.Secure.getInt(getContentResolver(), "navigation_mode") == 2;
+      } catch (Settings.SettingNotFoundException e) {
+        Log.d(TAG, e.toString());
+      }
+    }
+    return false;
+  }
+
   private boolean isHostDenied(String remoteHost) {
     return (remoteHost != null && permittedHost.get(remoteHost) != null && Boolean.FALSE.equals(permittedHost.get(remoteHost)));
   }
@@ -961,13 +973,15 @@ public class MainActivity extends NativeActivity {
   // Sets true full-screen on API 35+
   //
   private void setImmersiveMode() {
-    getWindow().getDecorView()
-      .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                             View.SYSTEM_UI_FLAG_FULLSCREEN |
-                             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    if (isGestureNavigationEnabled()) {
+      getWindow().getDecorView()
+                 .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
   }
 
   private void setupStorageEnvironment() {
